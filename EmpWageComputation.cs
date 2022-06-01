059f1cdeabb85f79627990bfe0c93f3d99dbfb9b
Saikumar <muthyalasaikumar9@gmail.com>
2022-06-01 21:00:18 +0530

    commit

---
+++ EmpWageComputation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWageComputationUsingMethods
+{
+    internal class EmpWageComputation
+    {
+        public void UC_1_EmpAttendance()
+        {
+            int isPresent = 1;
+            int randomCheck;
+            Random random = new Random();
+            randomCheck = random.Next(2);
+            if (randomCheck == isPresent)
+            {
+                Console.WriteLine("The Employee is Present ");
+            }
+            else
+            {
+                Console.WriteLine("The Employee is Absent ");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EmpWageComputationUsingMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------------- WELCOME TO THE EMPLOYEE WAGE COMPUTATION IN CLASSES AND METHODS --------------");
+            Console.WriteLine();
+
+            EmpWageComputation computation = new EmpWageComputation();
+            computation.UC_1_EmpAttendance();
+        }
+    }
+}
